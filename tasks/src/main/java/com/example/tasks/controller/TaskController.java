2f67561c214b3256399619bd2744deeeea09211c
Iulian Florea <iulian.florea4@gmail.com>
2024-03-16 17:59:37 +0200

    am facut controllerul pentru Task

--- tasks/src/main/java/com/example/tasks/controller/TaskController.java
+++ tasks/src/main/java/com/example/tasks/controller/TaskController.java
@@ -1,9 +1,11 @@
 package com.example.tasks.controller;
 
+import com.example.tasks.dto.TaskDto;
 import com.example.tasks.service.TaskService;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/task")
@@ -14,4 +16,31 @@ public class TaskController {
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
+
+    @PostMapping("/create")
+    public TaskDto create(@RequestBody TaskDto taskDto) {
+        return taskService.create(taskDto);
+    }
+
+    @GetMapping("/findById")
+    public TaskDto findById(@RequestParam Long id) {
+        return taskService.findById(id);
+    }
+
+    @GetMapping
+    public List<TaskDto> findAll() {
+       return taskService.findAll();
+    }
+
+    @PutMapping("/update")
+    public TaskDto update(@RequestBody TaskDto taskDto) {
+        return taskService.update(taskDto);
+    }
+
+    @DeleteMapping("/deleteById")
+    public void delete(@RequestParam Long id) {
+        taskService.delete(id);
+    }
+
+
 }
