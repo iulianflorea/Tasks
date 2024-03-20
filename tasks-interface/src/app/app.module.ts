6bcd2b5e33be006c6c95fe7d4ea7bd1526b7d2c4
Iulian Florea <iulian.florea4@gmail.com>
2024-03-20 22:13:00 +0200

    incerc sa fac componenta de register in frontend si nu merge

--- tasks-interface/src/app/app.module.ts
+++ tasks-interface/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
+import {RegisterComponent} from "./register/register.component";
+import {FormsModule} from '@angular/forms';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RegisterComponent,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- tasks-interface/src/app/register/register.component.ts
+++ tasks-interface/src/app/register/register.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,FormsModule
+  ]
 })
 export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({
+    firstname: new FormControl,
+    lastname: new FormControl,
+    email: new FormControl,
+    password: new FormControl
+  })
 
+  register() {
+
+  }
 }
