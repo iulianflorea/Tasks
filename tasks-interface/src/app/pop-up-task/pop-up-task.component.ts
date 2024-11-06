5c6849965d654e28f4846fbfcdfb0a8ab3c0fa00
Iulian Florea <iulian.florea4@gmail.com>
2024-11-06 08:28:22 +0200

    am creat componenta pop-up-task

---
+++ tasks-interface/src/app/pop-up-task/pop-up-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './pop-up-task.component.html',
+  styleUrl: './pop-up-task.component.css'
+})
+export class PopUpTaskComponent {
+
+}
